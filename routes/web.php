31a7995c840f2544ac9ae1d9797e409b625b95a9
Jobu4 <jobmutuku.k@gmail.com>
2024-09-09 11:26:36 +0300

    First commit, dynamic content and authentification system works

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//hotels
+Route::get('hotels/rooms/{id}', [App\Http\Controllers\Hotels\HotelsController::class, 'rooms'])->name('hotel.rooms');
