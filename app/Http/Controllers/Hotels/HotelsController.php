bdfc5e89fcae728c8dbeaebd664aa01e736a8139
Jobu4 <jobmutuku.k@gmail.com>
2024-09-11 12:50:51 +0300

    added booking functionality,

--- app/Http/Controllers/Hotels/HotelsController.php
+++ app/Http/Controllers/Hotels/HotelsController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\Hotels;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Apartment\Apartment;
+use App\Models\Booking\Booking;
+use App\Models\Hotel\Hotel;
+use Auth;
+
+use DateTime;
 
 class HotelsController extends Controller
 {
@@ -12,9 +17,54 @@ class HotelsController extends Controller
 
 
     public function rooms($id) {
+       
         $getRooms = Apartment:: select() ->orderBy('id', 'desc') -> take(6)
         ->where('hotel_id',$id)->get();
         return view('hotels.rooms', compact('getRooms'));
+    }
+
+    public function roomDetails($id) {
+        $getRoom = Apartment:: find($id);
+        return view('hotels.roomdetails', compact('getRoom'));
+    }
+
+    public function roomBooking(Request $request,$id) {
+        $room =Apartment::find($id);
+        $hotel =Hotel::find($id); 
+        // the n/j/Y  format removes the zeros on the date formats
+
+if(strval(date("n/j/Y")) < strval($request->check_in) AND strval(date("n/j/Y")) < strval($request->check_out)){
+//continue with the logic
+if($request->check_in < $request->check_out){
+    $datetime1 = new DateTime($request->check_in);
+    $datetime2=  new DateTime($request->check_out);
+    $interval = $datetime1->diff($datetime2);
+    $days = $interval->format('%a'); //now you can do whatever you want with the $days
+    //continue with the logic
+    $bookRooms = Booking::create([
+        "name" => $request->name,
+        "email" => $request->email,
+        "phone_number" => $request->phone_number,
+        "check_in" => $request->check_in,
+        "check_out" => $request->check_out,
+        "days" =>$days,
+        "price" => $days * $room->price,
+        "user_id" => Auth::user()->id,
+        "room_name" => $room->name,
+        "hotel_name" => $hotel->name,
+    ]);
+    
+    echo "You booked successfully.";
+
+}else{
+    echo "Check out date should be grater than check in date";
+}
+}else{
+    echo "Choose dates in the future, invalid check in or check out date";
+    // echo strval(date("n/j/Y"));
+    // echo strval($request->check_in);
+    // echo strval($request->check_out);
+}
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 //hotels
 Route::get('hotels/rooms/{id}', [App\Http\Controllers\Hotels\HotelsController::class, 'rooms'])->name('hotel.rooms');
+//hotels room details
+Route::get('hotels/rooms-details/{id}', [App\Http\Controllers\Hotels\HotelsController::class, 'roomDetails'])->name('hotel.rooms.details');
+//hotels room booking
+Route::post('hotels/rooms-booking/{id}', [App\Http\Controllers\Hotels\HotelsController::class, 'roomBooking'])->name('hotel.rooms.booking');
